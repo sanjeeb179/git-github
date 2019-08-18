e0bd8ff6e91d811bb8310862d1ae85736dafef6c
SANJEEBNAIK <SANJEEBNAIK@9.81.194.230>
2019-08-19 00:56:12 +0530

    added a print statement

--- gitSample/src/san/HelloGit.java
+++ gitSample/src/san/HelloGit.java
@@ -4,7 +4,11 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git !!. This is my first project.");
+		System.out.println("Hello Github");
 
 	}
+	
+	
+	
 
 }
